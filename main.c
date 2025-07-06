f286f733d08b804e119a72f3ff686f366dff343b
GuilhermeMiranda553 <guilherme.vmoliveira@hotmail.com>
2025-07-06 16:44:59 -0300

    Esboço do Main.c

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    printf("------------TRABALHO Estrutura de Dados I------------\n"
+           "Tema: Oficina\n\n");
+
+    int opcao = 0;
+    do {
+        printf("MENU\n"
+               "1 - Clientes\n"787
+        "2 - Agendamento\n"
+        "3 - Fila de Atendimento\n"
+        "4 - Histórico de Atendimento\n"
+        "5 - Relatório de Serviços\n"
+        "6 - Sair\n"
+        "Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                printf("Opção Inválida!\n");
+                break;
+                while (opcao != 6);
+
+        }
+        return 0;
+    }
+
