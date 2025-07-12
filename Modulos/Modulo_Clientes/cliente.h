ab610a585c0c323692f3533715c7fd452eade636
Fernando <fernandolamounier531@gmail.com>
2025-07-12 09:11:16 -0300

    Criação da tabela hash com busca e deleção de clientes, implementação de atualização e cadastro de clientes

--- Modulos/Modulo_Clientes/cliente.h
+++ Modulos/Modulo_Clientes/cliente.h
@@ -1,10 +1,15 @@
-//
-// Created by fernando on 7/9/25.
-//
-
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
-menuClientes()
+#include "cliente.h"
+#include "../TAD/TAD_Clientes/lista_Clientes.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void menuClientes(TabelaCliente *tabela);
+void pegarDadosCliente (TabelaCliente *tabela);
+void deletarCliente(TabelaCliente *tabela);
+void alterarCliente(TabelaCliente *tabela);
 
 #endif //CLIENTE_H
